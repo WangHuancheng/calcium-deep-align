0148809f42c8bc46c054d6f6a1550967065bef1d
wanghuancheng <wangkang090930@outlook.com>
2022-02-11 16:58:48 +0800

    finishi motion_generator

--- motion_generate.py
+++ motion_generate.py
@@ -3,10 +3,10 @@ import torch
 import torchfields
 
 class MotionGenerator(torch.nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,num_init_vector:int,map:torch.tensor) -> None:#num_vector:how many initial vectors are used to generate displacement field
         super().__init__()
-    
+        self.num_init_vector=num_init_vector
 
     def forward():
-        
+        torch.randn()
         pass
