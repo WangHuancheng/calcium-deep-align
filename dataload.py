cb25b6ec424a3fc5749cc7531cf0ea676e8f3a8d
wanghuancheng <wangkang090930@outlook.com>
2022-03-11 11:20:37 +0800

    data load

--- dataload.py
+++ dataload.py
@@ -7,19 +7,16 @@ from tifffile import imwrite as tiff_write
 from tqdm import trange
 
 def MyDataLoad():
-    for i in trange(4800):
-        input=torch.from_numpy(tiff_read(f'dataset1/train/wrapped/{i+1}_wra  pped.tif')).reshape(1,1,512,512)
-        if(i==0):
+    for i in trange(4800,6000):
+        input=torch.from_numpy(tiff_read(f'data/dataset1/test/gd/{i+1}_gd.tif')).reshape(1,1,512,512)
+        if(i==4800):
             tensor=input
         else:
             tensor = torch.cat((tensor,input))
     torch.save(tensor,'tensor.pt')
 
 if __name__ == "__main__":
-    #MyDataLoad()
-    tensor = torch.load('dataset1/train_gd.pt')
-    tensor/=2455 #maxinum of grey value
-    print('here')
-    dataset=torch.utils.data.TensorDataset(tensor)
+    MyDataLoad()
+    
     
 
